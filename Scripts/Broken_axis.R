02a2ea03baaf717b57630e281ccf853bd1c9185a
C. Li <47674556+cxli233@users.noreply.github.com>
2022-11-22 23:06:35 -0500

    Create Broken_axis.R
    Exam script to make graphs with broken axis.

---
+++ Scripts/Broken_axis.R
@@ -0,0 +1,71 @@
+
+library(tidyverse)
+library(patchwork)
+library(RColorBrewer)
+
+a <- data.frame(
+  x = c("a", "b", "c", "d", "e"),
+  y = c(10, 20, 30, 450, 500)
+)
+
+lower <- a %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_bar(stat = "identity", aes(fill = x),
+           width = 0.7, color = "black") +
+  geom_text(aes(label = y), vjust = -0.2) +
+  scale_fill_manual(values = brewer.pal(8, "Set2")) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.title.y = element_text(hjust =1 )) +
+  coord_cartesian(ylim = c(0, 50))
+
+upper <- a %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_bar(stat = "identity", aes(fill = x),
+           width = 0.7, color = "black") +
+  geom_text(aes(label = y), vjust = -0.2) +
+  scale_fill_manual(values = brewer.pal(8, "Set2")) +
+  labs(x = NULL,
+       y = NULL) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.line.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.x = element_blank()) +
+  coord_cartesian(ylim = c(400, 600))
+
+
+wrap_plots(upper, lower, nrow = 2) &
+  theme(axis.text = element_text(color = "black"),
+        text = element_text(size = 14))
+
+left <- a %>% 
+  ggplot(aes(x = y, y = x)) +
+  geom_bar(stat = "identity", aes(fill = x),
+           width = 0.7, color = "black") +
+  geom_text(aes(label = y), hjust = -0.2) +
+  scale_fill_manual(values = brewer.pal(8, "Set2")) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.title.x = element_text(hjust =1 )) +
+  coord_cartesian(xlim = c(0, 50))
+
+
+right <- a %>% 
+  ggplot(aes(x = y, y = x)) +
+  geom_bar(stat = "identity", aes(fill = x),
+           width = 0.7, color = "black") +
+  geom_text(aes(label = y),hjust = -0.2) +
+  scale_fill_manual(values = brewer.pal(8, "Set2")) +
+  labs(x = NULL,
+       y = NULL) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.text.y = element_blank()) +
+  coord_cartesian(xlim = c(400, 600))
+
+wrap_plots(left, right, nrow = 1) &
+  theme(axis.text = element_text(color = "black"),
+        text = element_text(size = 14))
